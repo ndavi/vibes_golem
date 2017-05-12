0dd7c41d795693bbd66f1e1a2d03e29c7db7b3ea
ndavi <nico01.david@gmail.com>
2017-05-12 02:33:46 +0200

    Suppression des commentaires d'exemple

--- .ino.cpp
+++ .ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-05-12 02:29:29
+//This file has been generated on 2017-05-12 02:33:05
 
 #include "Arduino.h"
 #include <SPI.h>
--- golem_vibes.ino
+++ golem_vibes.ino
@@ -1,20 +1,4 @@
-/*
 
- Udp NTP Client
-
- Get the time from a Network Time Protocol (NTP) time server
- Demonstrates use of UDP sendPacket and ReceivePacket
- For more on NTP time servers and the messages needed to communicate with them,
- see http://en.wikipedia.org/wiki/Network_Time_Protocol
-
- created 4 Sep 2010
- by Michael Margolis
- modified 9 Apr 2012
- by Tom Igoe
-
- This code is in the public domain.
-
- */
 
 #include <SPI.h>
 #include <WiFi.h>
